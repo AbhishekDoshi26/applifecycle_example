f45d3d43036c789c004640e049ca113e84e8b7f8
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2021-07-28 13:24:29 +0530

    override method to listen app lifecyle

--- lib/home.dart
+++ lib/home.dart
@@ -7,9 +7,32 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with WidgetsBindingObserver {
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    print('State: $state');
+    if (state == AppLifecycleState.resumed) print('App Resumed');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addObserver(this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('App Lifecycle!'),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
   }
 }
